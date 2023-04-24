2156b4c72fec8f726b3833ea67c70749b275fc5c
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 15:30:02 +0800

    process to extract even number

--- numbers_even_or_odd.py
+++ numbers_even_or_odd.py
@@ -6,7 +6,7 @@ with open("number.txt", "r") as number_file, open("even.txt", "a") as even_file,
         # create even.txt if number is even
         if number_txt_file % 2 == 0:
             even_file.write(str(number_txt_file) + "\n")
-        # extract from number.txt
+            # extract from number.txt
         # create other odd.txt if number is odd
         else:
             odd_file.write(str(number_txt_file) + "\n")
