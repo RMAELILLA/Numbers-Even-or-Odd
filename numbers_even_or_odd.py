709fe347b1e5aeae2a6932801fc9c4b0aaffef53
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 12:47:01 +0800

    create stepxtep process

---
+++ numbers_even_or_odd.py
@@ -0,0 +1,6 @@
+# read a number.txt
+# number.txt with 20 integers
+# create even.txt if number is even
+# extract from number.txt
+# create other odd.txt if number is odd
+# extract from number.txt
