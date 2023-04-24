32c064c1ed20de0a5736f227800080bbbc71c2f0
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 14:08:44 +0800

    still failed to write all even numbers in even.txt

--- numbers_even_or_odd.py
+++ numbers_even_or_odd.py
@@ -3,11 +3,13 @@
 with open("number.txt", "r") as number_file:
     for line in number_file:
         number_txt_file = int(line)
+        # create even.txt if number is even
         if number_txt_file % 2 == 0:
-            open("even.txt", "w")
+            even_file = number_txt_file
+            even_file = open("even.txt", "w")
+            even_file.write(str(number_txt_file) + "\n")
         else:
             print()
-# create even.txt if number is even
 # extract from number.txt
 # create other odd.txt if number is odd
 # extract from number.txt
