aaffd90c32029d82d393351ddded11dad39f5a3d
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 14:27:48 +0800

    decline a variable above to write in even, successfully run

--- numbers_even_or_odd.py
+++ numbers_even_or_odd.py
@@ -1,12 +1,10 @@
 # read a number.txt
 # number.txt with 20 integers
-with open("number.txt", "r") as number_file:
+with open("number.txt", "r") as number_file, open("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
     for line in number_file:
         number_txt_file = int(line)
         # create even.txt if number is even
         if number_txt_file % 2 == 0:
-            even_file = number_txt_file
-            even_file = open("even.txt", "w")
             even_file.write(str(number_txt_file) + "\n")
         else:
             print()
