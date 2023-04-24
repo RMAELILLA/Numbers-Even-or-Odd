ba3b9b5409480bae5b7caed9487b2a1b259ba4ae
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 13:13:56 +0800

    error, failed to write in even.txt

--- numbers_even_or_odd.py
+++ numbers_even_or_odd.py
@@ -1,9 +1,13 @@
 # read a number.txt
+# number.txt with 20 integers
 with open("number.txt", "r") as number_file:
     for line in number_file:
-        print(line.strip())
-    # number.txt with 20 integers
-    # create even.txt if number is even
-    # extract from number.txt
-    # create other odd.txt if number is odd
-    # extract from number.txt
+        number_txt_file = int(line)
+        if number_txt_file % 2 == 0:
+            open("even.txt", "w")
+        else:
+            print()
+# create even.txt if number is even
+# extract from number.txt
+# create other odd.txt if number is odd
+# extract from number.txt
