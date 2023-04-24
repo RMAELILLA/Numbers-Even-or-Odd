465e24e548f415dd3f116e1d4d4b5496c4cf8bad
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 14:31:44 +0800

    else write in odd.txt

--- numbers_even_or_odd.py
+++ numbers_even_or_odd.py
@@ -6,8 +6,8 @@ with open("number.txt", "r") as number_file, open("even.txt", "a") as even_file,
         # create even.txt if number is even
         if number_txt_file % 2 == 0:
             even_file.write(str(number_txt_file) + "\n")
+        # extract from number.txt
+        # create other odd.txt if number is odd
         else:
-            print()
-# extract from number.txt
-# create other odd.txt if number is odd
-# extract from number.txt
+            odd_file.write(str(number_txt_file) + "\n")
+        # extract from number.txt
