b9dd7be524d9ad9e7ac6e05cd2a96d2ecc35eddd
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 23:24:41 +0800

    rearranging comment and lines

--- numbers_even_or_odd.py
+++ numbers_even_or_odd.py
@@ -3,11 +3,11 @@
 with open("number.txt", "r") as number_file, open("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
     for line in number_file:
         number_txt_file = int(line)
-        # create even.txt if number is even
         if number_txt_file % 2 == 0:
-            even_file.write(str(number_txt_file) + "\n")
             # extract from number.txt
-        # create other odd.txt if number is odd
+            # create even.txt if number is even
+            even_file.write(str(number_txt_file) + "\n")
         else:
+            # extract from number.txt
+            # create other odd.txt if number is odd
             odd_file.write(str(number_txt_file) + "\n")
-        # extract from number.txt
